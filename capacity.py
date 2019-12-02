71dd598030d569a76d44b8411e549ac7effeeae7
Kody Johnson <kkjohns9@asu.edu>
2019-12-01 19:08:08 -0700

    update1

--- capacity.py
+++ capacity.py
@@ -7,10 +7,55 @@
 
 from sys import exit
 from collections import deque
+
+# Data File
+FILE = '/home/kody/myrepos/cse450/450_data.txt'
+
+# Plane Capacities
+A220 = 105
+A319 = 128
+A320 = 150
+A321 = 185
+A321neo = 196
+A330_200 = 230
+A330_300 = 290
+A330_900neo = 280
+A350_900 = 300
+B717_200 = 110
+B737_700 = 126
+B737_800 = 165
+B737_900 = 180
+B737_900ER = 180
+B737_Max9 = 180
+B757_200 = 180
+B757_300 = 230
+B767_300 = 200
+B767_300ER = 225
+B767_400ER = 240
+B777_200 = 270
+B777_200ER = 270
+B777_200LR = 280
+B777_300 = 300
+B787_8 = 235
+B787_9 = 280
+Embraer170 = 72
+Embraer175_E75 = 78
+Embraer190 = 100
+McDonnellDouglasMD_88 = 150
+McDonnellDouglasMD_90_30 = 150
+CRJ700 = 75
+CRJ900 = 75
+
 class Graph:
+
     def __init__(self, graph):
         self.graph = graph
         self.ROW = len(graph)
+
+    @staticmethod
+    def read_input_file():
+        f = open(FILE)
+        return 1
 
     # So this is Breadth-First-Search or BFS that will return true if a path exists from the s to t.
     def BFS(self, s, t, parent):
@@ -69,7 +114,10 @@ if __name__ == '__main__':
     # January 6th, 2020
     # Must depart and arrive on above date.
     # Aircraft Type -----------------
-    # TBA
+    # 737 - 165
+    # 32b - 185
+    # 321 - 185
+    #767-300  200
     # Cities ------------------------
     # LAX-Los Angeles
     # SFO-San Francisco
@@ -86,16 +134,17 @@ if __name__ == '__main__':
     # DL-Delta
     # UA-United
     # Hard code data into graph.
-    # 10 cities = 10 nodes
-    # each node has a city, arrival time, departure time, and edge weights or capacities of the aircraft type.
+    # Nodes =
+    # Each node represent a time and city
     graph_ = [[0, 16, 13, 0, 0, 0],
               [0, 0, 10, 12, 0, 0],
               [0, 4, 0, 0, 14, 0],
               [0, 0, 9, 0, 0, 20],
               [0, 0, 0, 7, 0, 4],
               [0, 0, 0, 0, 0, 0]]
+    new_graph = Graph.read_input_file()
     g = Graph(graph_)
     source_ = 0
     sink_ = 5
-    print("The maximum flow possible is", g.maximum_Flow(source_, sink_))
+    print("The maximum capacity is", g.maximum_Flow(source_, sink_))
     exit(0)
